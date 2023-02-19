0317239fa51b2531f5b285e5e6e27780dedbf721
Ayushman Singh <singhayushman13@protonmail.com>
2023-02-19 18:17:32 -0500

    add initial code file

---
+++ Part 1.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import os
+import argparse
+
+
+# In[13]:
+
+
+class IMEM(object):
+    def __init__(self, iodir):
+        self.size = pow(2, 16) # Can hold a maximum of 2^16 instructions.
+        self.filepath = os.path.abspath(os.path.join(iodir, "Code.asm"))
+        self.instructions = []
+
+        try:
+            with open(self.filepath, 'r') as insf:
+                self.instructions = [ins.strip() for ins in insf.readlines()]
+            print("IMEM - Instructions loaded from file:", self.filepath)
+            # print("IMEM - Instructions:", self.instructions)
+        except:
+            print("IMEM - ERROR: Couldn't open file in path:", self.filepath)
+
+    def Read(self, idx): # Use this to read from IMEM.
+        if idx < self.size:
+            return self.instructions[idx]
+        else:
+            print("IMEM - ERROR: Invalid memory access at index: ", idx, " with memory size: ", self.size)
+
+
+# In[14]:
+
+
+class DMEM(object):
+    # Word addressible - each address contains 32 bits.
+    def __init__(self, name, iodir, addressLen):
+        self.name = name
+        self.size = pow(2, addressLen)
+        self.min_value  = -pow(2, 31)
+        self.max_value  = pow(2, 31) - 1
+        self.ipfilepath = os.path.abspath(os.path.join(iodir, name + ".txt"))
+        self.opfilepath = os.path.abspath(os.path.join(iodir, name + "OP.txt"))
+        self.data = []
+
+        try:
+            with open(self.ipfilepath, 'r') as ipf:
+                self.data = [int(line.strip()) for line in ipf.readlines()]
+            print(self.name, "- Data loaded from file:", self.ipfilepath)
+            # print(self.name, "- Data:", self.data)
+            self.data.extend([0x0 for i in range(self.size - len(self.data))])
+        except:
+            print(self.name, "- ERROR: Couldn't open input file in path:", self.ipfilepath)
+
+    def Read(self, idx): # Use this to read from DMEM.
+        if idx < self.size:
+            return self.data(idx)
+        else:
+            f = open("SDMEM.txt", "r")
+
+    def Write(self, idx, val): # Use this to write into DMEM.
+        f = open("SDMEM.txt", "w")
+        for idx, i in enumerate(val):
+            f.write(str(self.data))
+        f.close()
+    
+    def dump(self):
+        try:
+            with open(self.opfilepath, 'w') as opf:
+                lines = [str(data) + '\n' for data in self.data]
+                opf.writelines(lines)
+            print(self.name, "- Dumped data into output file in path:", self.opfilepath)
+        except:
+            print(self.name, "- ERROR: Couldn't open output file in path:", self.opfilepath)
+
+
+# In[15]:
+
+
+class RegisterFile(object):
+    def __init__(self, name, count, length = 1, size = 32):
+        self.name       = name
+        self.reg_count  = count
+        self.vec_length = length # Number of 32 bit words in a register.
+        self.reg_bits   = size
+        self.min_value  = -pow(2, self.reg_bits-1)
+        self.max_value  = pow(2, self.reg_bits-1) - 1
+        self.registers  = [[0x0 for e in range(self.vec_length)] for r in range(self.reg_count)] # list of lists of integers
+
+    def Read(self, idx):
+        if idx < self.size:
+            return self.data(idx)
+        else:
+            f = open("VDMEM.txt", "r")
+        
+    def Write(self, idx, val):
+        f = open("VDMEM.txt", "w")
+        for idx, i in enumerate(val):
+            f.write(str(self.registers))
+        f.close()
+        
+    def dump(self, iodir):
+        opfilepath = os.path.abspath(os.path.join(iodir, self.name + ".txt"))
+        try:
+            with open(opfilepath, 'w') as opf:
+                row_format = "{:<13}"*self.vec_length
+                lines = [row_format.format(*[str(i) for i in range(self.vec_length)]) + "\n", '-'*(self.vec_length*13) + "\n"]
+                lines += [row_format.format(*[str(val) for val in data]) + "\n" for data in self.registers]
+                opf.writelines(lines)
+            print(self.name, "- Dumped data into output file in path:", opfilepath)
+        except:
+            print(self.name, "- ERROR: Couldn't open output file in path:", opfilepath)
+
+
+# In[16]:
+
+
+class Core():
+    def __init__(self, imem, sdmem, vdmem):
+        self.IMEM = imem
+        self.SDMEM = sdmem
+        self.VDMEM = vdmem
+
+        self.RFs = {"SRF": RegisterFile("SRF", 8),
+                    "VRF": RegisterFile("VRF", 8, 64)}
+
+    def SingleStageCore(Core):
+        def __init__(self, imem, sdmem, vdmem):
+            super(SingleStageCore, self)
+            self.opfilepath = {"SDMEMOP": RegisterFile("SDMEMOP", 64),
+                               "VDMEMOP": RegisterFile("VDMEMOP")}
+            self.pc = 0
+            
+    def run(self):
+        while not self.__done:
+            self.run()
+            self.dump()
+            self.stop()
+
+    def dumpregs(self, iodir):
+        for rf in self.RFs.values():
+            rf.dump(iodir)
+
+
+# In[17]:
+
+
+if __name__ == "__main__":
+    #parse arguments for input file location
+    parser = argparse.ArgumentParser(description='Vector Core Performance Model')
+    parser.add_argument('--iodir', default="", type=str, help='Path to the folder containing the input files - instructions and data.')
+    args = parser.parse_args()
+
+    iodir = os.path.abspath(args.iodir)
+    print("IO Directory:", iodir)
+
+    # Parse IMEM
+    imem = IMEM(iodir)  
+    # Parse SMEM
+    sdmem = DMEM("SDMEM", iodir, 13) # 32 KB is 2^15 bytes = 2^13 K 32-bit words.
+    # Parse VMEM
+    vdmem = DMEM("VDMEM", iodir, 17) # 512 KB is 2^19 bytes = 2^17 K 32-bit words. 
+
+    # Create Vector Core
+    vcore = Core(imem, sdmem, vdmem)
+
+    # Run Core
+    vcore.run()   
+    vcore.dumpregs(iodir)
+
+    sdmem.dump()
+    vdmem.dump()
+
+    # THE END
+
+
+# In[ ]:
+
+
+
+
