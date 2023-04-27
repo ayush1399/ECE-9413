b12f9f135a4f77d7fc94c3be4271edaa7f8f59a9
Ayushman Singh <singhayushman13@protonmail.com>
2023-04-26 21:21:43 -0400

    add functional code from Phase 1 to Phase 2

---
+++ Phase2/as16513_ra2466_funcsimulator.py
@@ -0,0 +1,190 @@
+import os
+import argparse
+
+from instructions import INSTRUCTION_SET
+
+class IMEM(object):
+    def __init__(self, iodir):
+        self.size = pow(2, 16) # Can hold a maximum of 2^16 instructions.
+        self.filepath = os.path.abspath(os.path.join(iodir, "Code.asm"))
+        self.instructions = []
+
+        try:
+            with open(self.filepath, 'r') as insf:
+                # self.instructions = [ins.strip() for ins in insf.readlines()]
+                self.instructions = [ins.split('#')[0].strip() for ins in insf.readlines() if not (ins.startswith('#') or ins.strip() == '')]
+            print(f"IMEM - Instructions loaded from file:{self.filepath}")
+            # print("IMEM - Instructions:", self.instructions)
+        except:
+            print(f"IMEM - ERROR: Couldn't open file in path:{self.filepath}")
+
+    def Read(self, idx): # Use this to read from IMEM.
+        if idx < self.size:
+            return self.instructions[idx]
+        else:
+            print(f"IMEM - ERROR: Invalid memory access at index: {idx} with memory size: {self.size}")
+
+class DMEM(object):
+    # Word addressible - each address contains 32 bits.
+    def __init__(self, name, iodir, addressLen):
+        self.name = name
+        self.size = pow(2, addressLen)
+        self.min_value  = -pow(2, 31)
+        self.max_value  = pow(2, 31) - 1
+        self.ipfilepath = os.path.abspath(os.path.join(iodir, name + ".txt"))
+        self.opfilepath = os.path.abspath(os.path.join(iodir, name + "OP.txt"))
+        self.data = []
+
+        try:
+            with open(self.ipfilepath, 'r') as ipf:
+                self.data = [int(line.strip()) for line in ipf.readlines()]
+                
+            print(f"{self.name}- Data loaded from file:{self.ipfilepath}")
+            # print(self.name, "- Data:", self.data)
+            self.data.extend([0x0 for i in range(self.size - len(self.data))])
+        except:
+            print(f"{self.name}- ERROR: Couldn't open input file in path:{self.ipfilepath}")
+
+    def Read(self, idx): # Use this to read from DMEM.
+        if 0 <= idx < self.size:
+            return self.data[idx]
+        else:
+            raise Exception(f"DMEM - ERROR: Invalid memory access at index: {idx} with memory size: {self.size}")
+
+    def Write(self, idx, val): # Use this to write into DMEM.
+        if 0 <= idx < self.size:
+            self.data[idx] = val
+        else:
+            raise Exception(f"DMEM - ERROR: Invalid memory access at index: {idx} with memory size: {self.size}")
+
+    def dump(self):
+        try:
+            with open(self.opfilepath, 'w') as opf:
+                lines = [str(data) + '\n' for data in self.data]
+                opf.writelines(lines)
+            print(self.name, "- Dumped data into output file in path:", self.opfilepath)
+        except:
+            print(self.name, "- ERROR: Couldn't open output file in path:", self.opfilepath)
+
+class RegisterFile(object):
+    def __init__(self, name, count, length = 1, size = 32):
+        self.name       = name
+        self.reg_count  = count
+        self.vec_length = length # Number of 32 bit words in a register.
+        self.reg_bits   = size
+        self.min_value  = -pow(2, self.reg_bits-1)
+        self.max_value  = pow(2, self.reg_bits-1) - 1
+        self.registers  = [[0x0 for e in range(self.vec_length)] for r in range(self.reg_count)] # list of lists of integers
+
+    def Read(self, idx):
+        try:
+            return self.registers[idx]
+        except IndexError:
+            raise Exception("Invalid register read.")
+
+    def Write(self, idx, val):
+        try:
+            register = self.registers[idx]
+            for i, v in enumerate(val):
+                register[i] = v
+        except IndexError:
+            raise Exception("Invalid register write.")
+
+    def dump(self, iodir):
+        opfilepath = os.path.abspath(os.path.join(iodir, self.name + ".txt"))
+        try:
+            with open(opfilepath, 'w') as opf:
+                row_format = "{:<13}"*self.vec_length
+                lines = [row_format.format(*[str(i) for i in range(self.vec_length)]) + "\n", '-'*(self.vec_length*13) + "\n"]
+                lines += [row_format.format(*[str(val) for val in data]) + "\n" for data in self.registers]
+                opf.writelines(lines)
+            print(self.name, "- Dumped data into output file in path:", opfilepath)
+        except:
+            print(self.name, "- ERROR: Couldn't open output file in path:", opfilepath)
+
+class Core():
+    def __init__(self, imem, sdmem, vdmem):
+        self.IMEM = imem
+        self.SDMEM = sdmem
+        self.VDMEM = vdmem
+
+        self.RFs = {"SRF": RegisterFile("SRF", 8),
+                    "VRF": RegisterFile("VRF", 8, 64)}
+        
+        self._VMR = [1 for _ in range(64)]
+        self._VLR = 64
+
+        self.__ISET = INSTRUCTION_SET
+        self._pc = 0
+        
+    def run(self):
+        try:
+            for INS, OPR in self.__exec:
+                if OPR:
+                    self[INS](self, *OPR)
+                else:
+                    self[INS](self)
+        except ValueError:
+            print(INS, OPR)
+
+    def dumpregs(self, iodir):
+        for rf in self.RFs.values():
+            rf.dump(iodir)
+
+    def _update_pc(self, i=1, _set=False):
+        if _set:
+            self._pc = i
+        else:
+            self._pc += i
+
+    def _register_read(self, R):
+        if R[0] == "S":
+            return self.RFs["SRF"].Read(int(R[2:]))[0]
+        elif R[0] == "V":
+            return self.RFs["VRF"].Read(int(R[2:]))
+    def _register_write(self, R, val):
+        if R[0] == "S":
+            self.RFs["SRF"].Write(int(R[2:]), [val])
+        elif R[0] == "V":
+            self.RFs["VRF"].Write(int(R[2:]), val)
+
+    @property
+    def __exec(self):
+        while self._pc is not None:
+            ins = self.IMEM.Read(self._pc).split(" ")
+            opr = ins[1:] if len(ins) > 1 else None
+            yield (ins[0], opr)
+
+    def __getitem__(self, name):
+        try:
+            return self.__ISET[name]
+        except KeyError:
+            print("Invalid instruction (core dumped)", name)
+
+if __name__ == "__main__":
+    #parse arguments for input file location
+    parser = argparse.ArgumentParser(description='Vector Core Performance Model')
+    parser.add_argument('--iodir', default="", type=str, help='Path to the folder containing the input files - instructions and data.')
+    args = parser.parse_args()
+
+    iodir = os.path.abspath(args.iodir)
+    print("IO Directory:", iodir)
+
+    # Parse IMEM
+    imem = IMEM(iodir)  
+    # Parse SMEM
+    sdmem = DMEM("SDMEM", iodir, 13) # 32 KB is 2^15 bytes = 2^13 K 32-bit words.
+    # Parse VMEM
+    vdmem = DMEM("VDMEM", iodir, 17) # 512 KB is 2^19 bytes = 2^17 K 32-bit words. 
+
+    # Create Vector Core
+    vcore = Core(imem, sdmem, vdmem)
+
+    # Run Core
+    vcore.run()   
+    vcore.dumpregs(iodir)
+
+    sdmem.dump()
+    vdmem.dump()
+
+    # THE END
---
+++ Phase2/instructions.py
@@ -0,0 +1,325 @@
+import functools
+
+INSTRUCTION_SET = dict()
+
+def InstructionWrap(ITYPE, I) -> int:
+    def WrappedI(*wargs):
+        return I(ITYPE, *wargs)
+    return WrappedI
+
+def int32(x):
+    x = x & 0xffffffff
+    return (x ^ 0x80000000) - 0x80000000
+
+def lrshift(val, n): return (val << n) & ((1 << 32) - 1)
+def rrshift(val, n): return (val % 0x100000000) >> n
+
+# =================
+# VECTOR OPERATIONS
+# =================
+
+
+# Vector Operations - VV
+def VOVV(VO, self, VR1, VR2, VR3):
+    VLR = self._VLR
+    VMR, VR2, VR3 = self._VMR[:VLR], self._register_read(VR2)[:VLR], self._register_read(VR3)[:VLR]
+
+    match VO:
+        case "ADD":
+            vr1 = list(map(lambda vr: int32(vr[0] + vr[1]), zip(VR2, VR3)))
+        case "SUB":
+            vr1 = list(map(lambda vr: int32(vr[0] - vr[1]), zip(VR2, VR3)))
+        case "MUL":
+            vr1 = list(map(lambda vr: int32(vr[0] * vr[1]), zip(VR2, VR3)))
+        case "DIV":
+            vr1 = list(map(lambda vr: int32(vr[0] // vr[1]), zip(VR2, VR3)))
+
+    VR1_MASKED = self._register_read(VR1)[:VLR]
+    VR1_MASKED = list(map(lambda vr: vr[2] if vr[1] else vr[0], zip(VR1_MASKED, VMR, vr1)))
+
+    self._register_write(VR1, VR1_MASKED)
+    self._update_pc()
+
+INSTRUCTION_SET["ADDVV"] = InstructionWrap("ADD", VOVV)
+INSTRUCTION_SET["SUBVV"] = InstructionWrap("SUB", VOVV)
+INSTRUCTION_SET["MULVV"] = InstructionWrap("MUL", VOVV)
+INSTRUCTION_SET["DIVVV"] = InstructionWrap("DIV", VOVV)
+
+
+# Vector Operations - VS
+def VOVS(VO, self, VR1, VR2, SR1):
+    VLR = self._VLR
+    VMR, VR2, SR1 = self._VMR[:VLR], self._register_read(VR2)[:VLR], self._register_read(SR1)
+
+    match VO:
+        case "ADD":
+            vr1 = list(map(lambda vr2_i: int32(vr2_i + SR1), VR2))
+        case "SUB":
+            vr1 = list(map(lambda vr2_i: int32(vr2_i - SR1), VR2))
+        case "MUL":
+            vr1 = list(map(lambda vr2_i: int32(vr2_i * SR1), VR2))
+        case "DIV":
+            vr1 = list(map(lambda vr2_i: int32(vr2_i / SR1), VR2))
+
+    VR1_MASKED = self._register_read(VR1)[:VLR]
+    VR1_MASKED = list(map(lambda vr: vr[2] if vr[1] else vr[0], zip(VR1_MASKED, VMR, vr1)))
+
+    self._register_write(VR1, VR1_MASKED)
+    self._update_pc()
+
+INSTRUCTION_SET["ADDVS"] = InstructionWrap("ADD", VOVS)
+INSTRUCTION_SET["SUBVS"] = InstructionWrap("SUB", VOVS)
+INSTRUCTION_SET["MULVS"] = InstructionWrap("MUL", VOVS)
+INSTRUCTION_SET["DIVVS"] = InstructionWrap("DIV", VOVS)
+
+
+# ===============================
+# VECTOR MASK REGISTER OPERATIONS
+# ===============================
+
+
+# Vector Mask Register Operations - VV
+def SVMROVV(VMRO, self, VR1, VR2):
+    VR1, VR2 = self._register_read(VR1), self._register_read(VR2)
+    match VMRO:
+        case "EQ":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i[0] == vr_i[1] else 0, zip(VR1, VR2)))
+        case "NE":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i[0] != vr_i[1] else 0, zip(VR1, VR2)))
+        case "GT":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i[0] > vr_i[1] else 0, zip(VR1, VR2)))
+        case "LT":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i[0] < vr_i[1] else 0, zip(VR1, VR2)))
+        case "GE":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i[0] >= vr_i[1] else 0, zip(VR1, VR2)))
+        case "LE":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i[0] <= vr_i[1] else 0, zip(VR1, VR2)))
+    self._update_pc()
+
+INSTRUCTION_SET["SEQVV"] = InstructionWrap("EQ", SVMROVV)
+INSTRUCTION_SET["SNEVV"] = InstructionWrap("NE", SVMROVV)
+INSTRUCTION_SET["SGTVV"] = InstructionWrap("GT", SVMROVV)
+INSTRUCTION_SET["SLTVV"] = InstructionWrap("LT", SVMROVV)
+INSTRUCTION_SET["SGEVV"] = InstructionWrap("GE", SVMROVV)
+INSTRUCTION_SET["SLEVV"] = InstructionWrap("LE", SVMROVV)
+
+# Vector Mask Register Operations - VS
+def SVMROVS(VMRO, self, VR1, SR1):
+    VR1, SR1 = self._register_read(VR1), self._register_read(SR1)
+    match VMRO:
+        case "EQ":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i == SR1 else 0, VR1))
+        case "NE":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i != SR1 else 0, VR1))
+        case "GT":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i > SR1 else 0, VR1))
+        case "LT":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i < SR1 else 0, VR1))
+        case "GE":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i >= SR1 else 0, VR1))
+        case "LE":
+            self._VMR = list(map(lambda vr_i: 1 if vr_i <= SR1 else 0, VR1))
+    self._update_pc()
+
+INSTRUCTION_SET["SEQVS"] = InstructionWrap("EQ", SVMROVS)
+INSTRUCTION_SET["SNEVS"] = InstructionWrap("NE", SVMROVS)
+INSTRUCTION_SET["SGTVS"] = InstructionWrap("GT", SVMROVS)
+INSTRUCTION_SET["SLTVS"] = InstructionWrap("LT", SVMROVS)
+INSTRUCTION_SET["SGEVS"] = InstructionWrap("GE", SVMROVS)
+INSTRUCTION_SET["SLEVS"] = InstructionWrap("LE", SVMROVS)
+
+# Vector Mask Register Operations - CVM
+def CVM(self):
+    self._VMR = [1 for _ in range(64)]
+    self._update_pc()
+INSTRUCTION_SET["CVM"] = CVM
+
+# Vector Mask Register Operations - POP
+def POP(self, SR1):
+    sum = functools.reduce(lambda acc, val: acc + val, self._VMR)
+    self._register_write(SR1, sum)
+    self._update_pc()
+INSTRUCTION_SET["POP"] = POP
+
+
+# =================================
+# VECTOR LENGTH REGISTER OPERATIONS
+# =================================
+
+
+# Vector Length Register Operations - MTCL
+def MTCL(self, SR1):
+    self._VLR = self._register_read(SR1)
+    self._update_pc()
+INSTRUCTION_SET["MTCL"] = MTCL
+
+# Vector Length Register Operations - MFCL
+def MFCL(self, SR1):
+    self._register_write(SR1, self._VLR)
+    self._update_pc()
+INSTRUCTION_SET["MFCL"] = MFCL
+
+
+# ========================
+# MEMORY ACCESS OPERATIONS
+# ========================
+
+
+# Memory Access Operations - 11
+def LV(self, VR1, SR1):
+    VLR, SR1 = self._VLR, self._register_read(SR1)
+    vr1 = list(map(lambda idx: self.VDMEM.Read(SR1+idx), range(VLR)))
+    VMR = self._VMR[:VLR]
+    VR1_MASKED = self._register_read(VR1)[:VLR]
+    VR1_MASKED = list(map(lambda vr: vr[2] if vr[1] else vr[0], zip(VR1_MASKED, VMR, vr1)))
+    self._register_write(VR1, VR1_MASKED)
+    self._update_pc()
+INSTRUCTION_SET["LV"] = LV
+
+# Memory Access Operations - 12
+def SV(self, VR1, SR1):
+    VLR, SR1, VR1 = self._VLR, self._register_read(SR1), self._register_read(VR1)
+    VMR = self._VMR[:VLR]
+    for idx in range(VLR):
+        if VMR[idx]:
+            self.VDMEM.Write(SR1+idx, VR1[idx])
+    self._update_pc()
+INSTRUCTION_SET["SV"] = SV
+
+# Memory Access Operations - 13
+def LVWS(self, VR1, SR1, SR2):
+    VLR, SR1, SR2 = self._VLR, self._register_read(SR1), self._register_read(SR2)
+    vr1 = list(map(lambda idx: self.VDMEM.Read(SR1+idx), range(0, VLR, SR2)))
+    VMR = self._VMR[:VLR]
+    VR1_MASKED = self._register_read(VR1)[:VLR]
+    VR1_MASKED = list(map(lambda vr: vr[2] if vr[1] else vr[0], zip(VR1_MASKED, VMR, vr1)))
+    self._register_write(VR1, VR1_MASKED)
+    self._update_pc()
+INSTRUCTION_SET["LVWS"] = LVWS
+
+# Memory Access Operations - 14
+def SVWS(self, VR1, SR1, SR2):
+    VLR, VR1 =  self._VLR, self._register_read(VR1) 
+    SR1, SR2 = self._register_read(SR1), self._register_read(SR2)
+    VMR = self._VMR[:VLR]
+    for idx in range(VLR):
+        if (VMR[idx]):
+            self.VDMEM.Write(SR1+idx*SR2, VR1[idx])
+    self._update_pc()
+INSTRUCTION_SET["SVWS"] = SVWS
+
+# Memory Access Operations - 15
+def LVI(self, VR1, SR1, VR2):
+    VLR, SR1, VR2 = self._VLR, self._register_read(SR1), self._register_read(VR2)
+    vr1 = list(map(lambda idx: self.VDMEM.Read(SR1+VR2[idx]), range(VLR)))
+    VMR = self._VMR[:VLR]
+    VR1_MASKED = self._register_read(VR1)[:VLR]
+    VR1_MASKED = list(map(lambda vr: vr[2] if vr[1] else vr[0], zip(VR1_MASKED, VMR, vr1)))
+    self._register_write(VR1, VR1_MASKED)
+    self._update_pc()
+INSTRUCTION_SET["LVI"] = LVI
+
+# Memory Access Operations - 16
+def SVI(self, VR1, SR1, VR2):
+    VLR, VR1, SR1, VR2 = self._VLR, self._register_read(VR1), self._register_read(SR1), self._register_read(VR2)
+    VMR = self._VMR[:VLR]
+    for idx in range(VLR):
+        if (VMR[idx]):
+            self.VDMEM.Write(SR1+VR2[idx], VR1[idx])
+    self._update_pc()
+INSTRUCTION_SET["SVI"] = SVI
+
+# Memory Access Operations - 17
+def LS(self, SR2, SR1, Imm):
+    SR1 = self._register_read(SR1)
+    self._register_write(SR2, self.SDMEM.Read(SR1 + int(Imm)))
+    self._update_pc()
+INSTRUCTION_SET["LS"] = LS
+
+# Memory Access Operations - 18
+def SS(self, SR2, SR1, Imm):
+    SR1, SR2 = self._register_read(SR1), self._register_read(SR2)
+    self.SDMEM.Write(SR1+int(Imm), SR2)
+    self._update_pc()
+INSTRUCTION_SET["SS"] = SS
+
+
+# =================
+# SCALAR OPERATIONS
+# =================
+
+
+def SO(SOP, self, SR3, SR1, SR2):
+    SR1, SR2 = self._register_read(SR1), self._register_read(SR2)
+    match SOP:
+        case "ADD":
+            sr3 = int32(SR1 + SR2)
+        case "SUB":
+            sr3 = int32(SR1 - SR2)
+        case "AND":
+            sr3 = int32(SR1 & SR2)
+        case "OR":
+            sr3 = int32(SR1 | SR2)
+        case "XOR":
+            sr3 = int32(SR1 ^ SR2)
+        case "SLL":
+            sr3 = lrshift(SR1, SR2)
+        case "SRL":
+            sr3 = rrshift(SR1, SR2)
+        case "SRA":
+            sr3 = int32(SR1 >> SR2)
+    self._register_write(SR3, sr3)
+    self._update_pc()
+
+INSTRUCTION_SET["ADD"] = InstructionWrap("ADD", SO)
+INSTRUCTION_SET["SUB"] = InstructionWrap("SUB", SO)
+INSTRUCTION_SET["AND"] = InstructionWrap("AND", SO)
+INSTRUCTION_SET["OR"] = InstructionWrap("OR", SO)
+INSTRUCTION_SET["XOR"] = InstructionWrap("XOR", SO)
+INSTRUCTION_SET["SLL"] = InstructionWrap("SLL", SO)
+INSTRUCTION_SET["SRL"] = InstructionWrap("SRL", SO)
+INSTRUCTION_SET["SRA"] = InstructionWrap("SRA", SO)
+
+
+# =======
+# CONTROL
+# =======
+
+
+def B(OP, self, SR1, SR2, Imm):
+    SR1, SR2 = self._register_read(SR1), self._register_read(SR2)
+    flag = False
+    match OP:
+        case "EQ":
+            flag = (SR1 == SR2)
+        case "NE":
+            flag = (SR1 != SR2)
+        case "GT":
+            flag = (SR1 > SR2)
+        case "LT":
+            flag = (SR1 < SR2)
+        case "GE":
+            flag = (SR1 >= SR2)
+        case "LE":
+            flag = (SR1 <= SR2)
+    if flag:
+        self._update_pc(int(Imm))
+    else:
+        self._update_pc()
+
+INSTRUCTION_SET["BEQ"] = InstructionWrap("EQ", B)
+INSTRUCTION_SET["BNE"] = InstructionWrap("NE", B)
+INSTRUCTION_SET["BGT"] = InstructionWrap("GT", B)
+INSTRUCTION_SET["BLT"] = InstructionWrap("LT", B)
+INSTRUCTION_SET["BGE"] = InstructionWrap("GE", B)
+INSTRUCTION_SET["BLE"] = InstructionWrap("LE", B)
+
+
+# ====
+# HALT
+# ====
+
+# Halt - 24
+def HALT(self):
+    self._update_pc(None, True)
+INSTRUCTION_SET["HALT"] = HALT
